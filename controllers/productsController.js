48643e1c15e3391b2189845e86c53c7592c37ccc
AlexDDks <magarciaa92@gmail.com>
2022-03-23 20:21:32 -0600

    Se ha creado la funcionalidad de crear y agregar productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -20,7 +20,18 @@ const controller = {
         res.render('product-create-form')
     },
     store: (req, res) => {
-        const body = req.body //Esto se hace para obtener todos los datos de cada clase que se nos envía desde el formulario
+        const newProduct = {
+
+            //Al new body le faltan 2 campos de la base de datos, el id y la imagen por lo que vamos a ponerlos:
+            id: products[products.length - 1].id + 1, //el array de la base de datos tiene 8 elementos, nos queremos posicionar en el índice 7(el último del array) por lo tanto si el length es = 8, al restar 1 llegamos a la posición 7 del array que corresponde al último elemento
+            ...req.body //sintaxis spread nos permite tener todos los elementos del objeto body body.name, body.price... sin tener que escribir 1 por 1
+            //Así el new product va a tener todo lo del body
+        }
+        //En este punto ya se tiene el objeto creado en el índice último + 1, ahora se tiene que mandar a la base de datos (JSON), mediante el PUSH
+        products.push(newProduct)
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '))
+
         res.redirect('/products') //Una vez que se envían los datos se tiene que redireccionar a una dirección con el path completo, en este caso regresamos a la ventana de productos
     }
 
