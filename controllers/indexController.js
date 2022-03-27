14810394883ca80e365a18bf105fe4d088662d5c
agarduno87 <ing.antoniogz@gmail.com>
2022-03-26 18:35:18 -0600

    instalacion de dependencias y modificación del indes.js

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -35,9 +35,9 @@ const controller = {
   burger: (req, res, next) => {
     res.render('burger');
   }
-  create: (req, res, next) => {
-    res.render('create');
-  }
+  //create: (req, res, next) => {
+    //res.render('create');
+  //}
 
 };
 
