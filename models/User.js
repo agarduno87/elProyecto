fe8def49a4c21ede64b7df2914e149029ec3d96f
agarduno87 <ing.antoniogz@gmail.com>
2022-03-28 17:56:07 -0600

    se mejoro el .sequelizerc y se trató de hacer la estructura del CRUD en el archivo User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,50 @@
+// guardar el usuario en la db
 const fs = require('fs');
 const User = {
-    fileName: './data/users.json',
+    fileName: './data/user.json',
     getData: function () {
         return fs.readFileSync(this.fileName, 'utf-8');
     },
+    
     create: function (userData) {
-        const users = JSON.parse(this.getData());
+        let data = this.getData();
+        let users = JSON.parse(data);
         users.push(userData);
         fs.writeFileSync(this.fileName, JSON.stringify(users));
+
+    },
+    
+    update: function (userData) {
+        let data = this.getData();
+        let users = JSON.parse(data);
+        users.forEach(function (user, index) {
+            if (user.id === userData.id) {
+                users[index] = userData;
+            }
+        }  
+        );
+        fs.writeFileSync(this.fileName, JSON.stringify(users));
+    },
+    
+    delete: function (userData) {
+        let data = this.getData();
+        let users = JSON.parse(data);
+        users.forEach(function (user, index) {
+            if (user.id === userData.id) {
+                users.splice(index, 1);
+            }
+        }  
+        );
+        fs.writeFileSync(this.fileName, JSON.stringify(users));
+    },
+    
+    read: function (userData) {
+        let data = this.getData();
+        let users = JSON.parse(data);
+        let user = users.find(function (user) {
+            return user.id === userData.id;
+        });
+        return user;
     }
 }
 
